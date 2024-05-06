97f49cb45e16dcb4a80467432d24c92575e41d74
THEmicrob <louise.dompnier@epfl.ch>
2024-05-06 19:55:34 +0200

    a

--- propagation_constante/a/nx.py
+++ propagation_constante/a/nx.py
@@ -9,18 +9,18 @@ executable = 'ex5.exe'  # Name of the compiled code
 input_filename = 'propagation_constante\\a\\droite.in' # Input file
 
 nsteps=5000
-nx=np.linspace(0,1000,10)
+nx=np.array([10,15,30,100])
 
 paramstr = 'nx'  # Parameter name to scan
 param=nx
 
 # Simulations
-# for j in range(len(param)):
-#     output_file = f"propagation_constante/a/{paramstr}={param[j]}_{j}.out"
-#     cmd = f"{repertoire}{executable} {input_filename} {paramstr}={param[j]:.15g} output={output_file}"
-#     print(cmd)
-#     subprocess.run(cmd, shell=True)
-#     print('Done.')
+for j in range(len(param)):
+    output_file = f"propagation_constante/a/{paramstr}={param[j]}_{j}.out"
+    cmd = f"{repertoire}{executable} {input_filename} {paramstr}={param[j]:.15g} output={output_file}"
+    print(cmd)
+    subprocess.run(cmd, shell=True)
+    print('Done.')
 
 # output_file = f"propagation_constante/a/{paramstr}={param}.out"
 # cmd = f"{repertoire}{executable} {input_filename} {paramstr}={param:.15g} output={output_file}"
@@ -28,7 +28,8 @@ param=nx
 # subprocess.run(cmd, shell=True)
 # print('Done.')
 
-colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'w', 'coral', 'lime', 'aqua', 'fuchsia', 'yellow', 'black', 'red', 'green', 'blue', 'purple', 'orange', 'brown', 'pink', 'gray', 'olive', 'cyan', 'navy', 'teal', 'violet', 'magenta', 'turquoise', 'tan', 'salmon', 'gold', 'indigo', 'maroon', 'ivory', 'chartreuse', 'wheat', 'azure', 'lavender', 'rose', 'lemon', 'scarlet', 'sand', 'cream', 'khaki', 'plum', 'orchid', 'lime', 'pear', 'cerulean', 'slate', 'steel', 'apricot', 'brass', 'bronze']
+colors = ['coral', 'black', 'red', 'blue', 'purple', 'orange', 'brown', 'pink', 'gray', 'olive', 'cyan', 'navy', 'teal', 'violet', 'magenta', 'turquoise', 'tan', 'salmon', 'gold', 'indigo', 'maroon', 'ivory', 'chartreuse', 'wheat', 'azure', 'lavender', 'rose', 'lemon', 'scarlet', 'sand', 'cream', 'khaki', 'plum', 'orchid', 'lime', 'pear', 'cerulean', 'slate', 'steel', 'apricot', 'brass', 'bronze']
+fs=15
 
 # Analyse
 for j in range(len(param)):
@@ -39,13 +40,15 @@ for j in range(len(param)):
     data_f=np.loadtxt(lbl_f)
     data_v=np.loadtxt(lbl_v)
     data_x=np.loadtxt(lbl_x)
-    plt.plot(data_x, np.delete(data_f[2,:],0), label=f"nx={param[j]}", color=colors[j])
+    plt.plot(data_x, np.delete(data_f[2,:],0), label=f"nx={int(round(param[j],0))}", color=colors[j])
     print(f"nx={param[j]}")
-plt.legend()
+plt.legend(fontsize=fs-3)
 # artists = [ plt.plot(data_x, np.delete(data_f[i,:],0), label=f"t={i}") for i in range(0, nsteps, 100)]
-
+plt.xlabel('x',fontsize=fs)
+plt.ylabel('y',fontsize=fs)
 # plot
-fig=plt.figure()
 # image=ArtistAnimation(fig, artists, interval=100, blit=True)
 # image.save(f"propagation_constante/a/{paramstr}={param[j]}_{j}.gif", writer='imagemagick')
+plt.tight_layout()
+plt.savefig(f"propagation_constante/a/{paramstr}={param[j]}_{j}.png",dpi=200)
 plt.show()
